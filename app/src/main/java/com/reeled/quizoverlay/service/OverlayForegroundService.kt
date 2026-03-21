a1420ac63409aa87deb9bcac11b77f3fb0aaf76f
shyam-Repeat <shyammostking@gmail.com>
2026-03-21 15:29:05 +0530

    Fix remaining overlay Compose import issues

--- app/src/main/java/com/reeled/quizoverlay/service/OverlayForegroundService.kt
+++ app/src/main/java/com/reeled/quizoverlay/service/OverlayForegroundService.kt
@@ -14,6 +14,7 @@ import android.view.Gravity
 import android.view.WindowManager
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment
--- app/src/main/java/com/reeled/quizoverlay/ui/overlay/cards/TapChoiceCard.kt
+++ app/src/main/java/com/reeled/quizoverlay/ui/overlay/cards/TapChoiceCard.kt
@@ -10,12 +10,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope

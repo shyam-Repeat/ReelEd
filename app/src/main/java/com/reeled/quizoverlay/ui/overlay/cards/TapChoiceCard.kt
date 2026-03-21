b74b1b36a94ac5d4310a9a9723ed068131cd9f36
shyam-Repeat <shyammostking@gmail.com>
2026-03-21 19:42:55 +0530

    Fix missing Compose imports in overlay cards

--- app/src/main/java/com/reeled/quizoverlay/ui/overlay/cards/TapChoiceCard.kt
+++ app/src/main/java/com/reeled/quizoverlay/ui/overlay/cards/TapChoiceCard.kt
@@ -1,6 +1,7 @@
 package com.reeled.quizoverlay.ui.overlay.cards
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -22,6 +23,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow

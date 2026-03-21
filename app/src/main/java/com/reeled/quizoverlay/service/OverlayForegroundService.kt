934e2a4f270fb3576f0102c14f449c4f364b263f
shyam-Repeat <shyammostking@gmail.com>
2026-03-21 15:40:29 +0530

    Add missing size import for overlay service

--- app/src/main/java/com/reeled/quizoverlay/service/OverlayForegroundService.kt
+++ app/src/main/java/com/reeled/quizoverlay/service/OverlayForegroundService.kt
@@ -15,6 +15,7 @@ import android.view.WindowManager
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Alignment

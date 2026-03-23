2eecd0d5d4e078b532a43e167991fd3112edca4a
shyam-Repeat <shyammostking@gmail.com>
2026-03-23 20:06:58 +0530

    Adjust overlay mascots and add drag-drop TTS

--- app/src/main/java/com/reeled/quizoverlay/ui/overlay/components/ModernQuizBackground.kt
+++ app/src/main/java/com/reeled/quizoverlay/ui/overlay/components/ModernQuizBackground.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color

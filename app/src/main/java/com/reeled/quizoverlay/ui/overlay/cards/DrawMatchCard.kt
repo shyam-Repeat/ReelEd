5331d1512590609031d6f2d89688c869b57b0518
Subhashri2 <143712109+Subhashri2@users.noreply.github.com>
2026-04-06 14:55:17 +0000

    bf2

--- app/src/main/java/com/reeled/quizoverlay/ui/overlay/cards/DrawMatchCard.kt
+++ app/src/main/java/com/reeled/quizoverlay/ui/overlay/cards/DrawMatchCard.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
